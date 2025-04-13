6076556fc0483c86d62ce27f21a11580d1ca5c39
Kristian Hafell <146472912+KristianHafell@users.noreply.github.com>
2025-04-13 11:35:53 +0200

    fix(view): fix rotation bug

--- view/view.js
+++ view/view.js
@@ -62,6 +62,10 @@ export class View {
         });
     }
 
+    norm_angle (angle) {
+        return (angle + 2 * Math.PI) % (2 * Math.PI);
+    }
+
     add_obstacles(view_objects) {
         const player_pos = this.model.player.pos;
         const player_rot = this.model.player.rot;
@@ -71,14 +75,9 @@ export class View {
         for (const ob of Object.values(this.model.obstacles)) {
             const [ angle, dist ] = this.angle_and_dist(player_pos, ob.pos);
 
-            for (const t of [0, 2 * Math.PI, -2 * Math.PI]) {
-                const x_pos = (angle - pd_min + t) / fov * size;
-                if (0 <= x_pos && x_pos <= size) {
-                    const { x, y, w, h } = this.get_w(this.images[ob.name], x_pos, this.get_height(dist));
-                    view_objects.push(new ViewObject("surface", [x, y], [w, h], dist, null, null, this.images[ob.name]));
-                    break;
-                }
-            }
+            const x_pos = (angle - this.norm_angle(pd_min)) / fov * size;
+            const { x, y, w, h } = this.get_w(this.images[ob.name], x_pos, this.get_height(dist));
+            view_objects.push(new ViewObject("surface", [x, y], [w, h], dist, null, null, this.images[ob.name]));
         }
     }
     get_w(image, x_pos, height) {
