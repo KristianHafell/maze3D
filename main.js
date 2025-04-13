9cb8c73264984b7c31267b4020e5e5ecf607dc7c
Kristian Hafell <146472912+KristianHafell@users.noreply.github.com>
2025-04-13 07:53:14 +0200

    feat(js): moving box

--- main.js
+++ main.js
@@ -2,3 +2,39 @@ const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 const size = canvas.width;
 
+const keys = {};
+
+document.addEventListener("keydown", e => keys[e.key.toLowerCase()] = true);
+document.addEventListener("keyup", e => keys[e.key.toLowerCase()] = false);
+
+class Box {
+    constructor() {
+        this.pos = [size / 2, size / 2];
+        this.speed = 5;
+    }
+
+    move() {
+        if (keys["w"]) this.pos[1] -= this.speed;
+        if (keys["s"]) this.pos[1] += this.speed;
+        if (keys["a"]) this.pos[0] -= this.speed;
+        if (keys["d"]) this.pos[0] += this.speed;
+    }
+
+    draw() {
+        ctx.fillStyle = "blue";
+        ctx.fillRect(this.pos[0] - 10, this.pos[1] - 10, 20, 20);
+    }
+}
+
+const box = new Box();
+
+function gameLoop() {
+    box.move();
+
+    ctx.clearRect(0, 0, size, size);
+    box.draw();
+
+    requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
