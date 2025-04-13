1507f8afd498c6f6575847d0fa03e3073efec51c
Kristian Hafell <146472912+KristianHafell@users.noreply.github.com>
2025-04-13 09:21:01 +0200

    feat(view): add 3D view

--- view/view.js
+++ view/view.js
@@ -2,6 +2,8 @@ const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 const size = canvas.width;
 
+const eu = Math.exp(Math.log(700) / 10);
+
 export class View {
     constructor(model) {
         this.model = model;
@@ -10,9 +12,10 @@ export class View {
     draw() {
         ctx.clearRect(0, 0, size, size);
 
+        this.draw_rays();
+
         const p2 = [this.model.player.pos[0]*10 + Math.cos(this.model.player.rot) * 4, this.model.player.pos[1]*10 + Math.sin(this.model.player.rot) * 4]
         this.draw_line(this.model.player.pos.map(n => n * 10), p2, "red", 3);
-
         this.draw_circle(this.model.player.pos.map(n => n * 10), 3, "white");
         
         for (let i = 0; i < this.model.maze.layout.length; i++) {
@@ -22,6 +25,10 @@ export class View {
                 }
             }
         }
+    }
+
+    get_height(dist) {
+        return Math.pow(eu, 10 - (dist - 1));
     }
 
     draw_line(p1, p2, color="black", width=1) {
@@ -43,5 +50,20 @@ export class View {
     draw_rectangle(p, w, h, color="black") {
         ctx.fillStyle = color;
         ctx.fillRect(p[0], p[1], w, h);
+    }
+    draw_rays() {
+        // Draw rays cast by the player.
+        const res = this.model.player.res;
+        const min_fov = this.model.player.min_FOV;
+        const max_fov = this.model.player.max_FOV;
+        this.model.player.ray(this.model.maze.layout).forEach((wall, i) => {
+            const height = this.get_height(wall[0]);
+            this.draw_rectangle(
+                [i * (size / res + 1), Math.floor((size - height) / 2)],
+                size / res + 1,
+                height,
+                `rgb(0, 0, ${Math.floor(255 * (height / size))})`
+            );
+        });
     }
 }
